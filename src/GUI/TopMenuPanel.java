317dff73d32cc662445406af9779c3138975d3ab
lucas-borges <lucasrborges94@gmail.com>
2015-05-26 23:07:48 -0300

    tentando listener topmenu

--- src/GUI/TopMenuPanel.java
+++ src/GUI/TopMenuPanel.java
@@ -1,18 +1,38 @@
 package GUI;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class TopMenuPanel {
+public class TopMenuPanel implements ActionListener {
 
+	private static JButton newGameBut;
+	
+	
 	private TopMenuPanel(){
-		
 	}
 	
 	static public JPanel getGUI(){
 		JPanel p=new JPanel();
 		
-		JButton newGameBut = new JButton("Novo Jogo");
+		newGameBut = new JButton("Novo Jogo");
+		newGameBut.setActionCommand("newGame");
+		
+		//Listen
+		newGameBut.addActionListener(TopMenuPanel);		
+		
+		//Add components
 		p.add(newGameBut);
+		
+		
 		return p;
 	}
+	
+	public void actionPerformed(ActionEvent e){
+		if("newGame".equals(e.getActionCommand())){
+			JOptionPane.showMessageDialog(null,"novo jogo criado");
+		}
+	}
+	
 }
